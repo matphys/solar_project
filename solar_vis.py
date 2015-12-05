0595db6b9c7a0aab96eec23b82170686fddb0a05
azat <razilya5858@yandex.ru>
2015-12-05 19:10:38 +0300

    Update solar_vis.py

--- solar_vis.py
+++ solar_vis.py
@@ -50,7 +50,7 @@ def scale_y(y):
     **y** — y-координата модели.
     """
 
-    return int(y*scale_factor)+window_height//2  # FIXME: not done yet
+    return int(-y*scale_factor) + window_height//2
 
 
 def create_star_image(space, star):
@@ -63,7 +63,7 @@ def create_star_image(space, star):
     x = scale_x(star.x)
     y = scale_y(star.y)
     r = star.R
-    star.image = space.create_oval([x - r, y - r], [x + r, y +r],fill=star.color)
+    star.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=star.color)
 
 
 def create_planet_image(space, planet):
@@ -75,8 +75,7 @@ def create_planet_image(space, planet):
     x = scale_x(planet.x)
     y = scale_y(planet.y)
     r = planet.R
-    planet.image = space.create_oval([x-r,y-r],[x+r,y+r],fill=planet.color)
-    #pass  # FIXME: сделать как у звезды
+    planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
 
 
 def update_system_name(space, system_name):
@@ -106,3 +105,4 @@ def update_object_position(space, body):
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
+ 
